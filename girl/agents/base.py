3898435680e5f33acc22fb119bdcd28b3710e182
ZiwenZhuang <leo416850130@outlook.com>
2020-04-05 00:10:14 +0800

    submit code framework 3: wait for further changes

--- girl/agents/base.py
+++ girl/agents/base.py
@@ -2,6 +2,17 @@
 class AgentBase:
     """ Basic interface of agents
     """
+    def state_dict(self):
+        """ return a state dicts that stores model parameters, just like torch.Tensor.state_dict()
+        """
+        return dict()
+
+    def load_state_dict(self, state_dict):
+        """ given a state_dict with the same format this instance outputs, load snapshot to this
+        instance.
+        """
+        pass
+
     def step(self, observation):
         """ Given the observation, the agent outputs the action. For recurrent agent, it stores
         hidden states itself, and abandom them via reset.

cddaab8ea3725a978fb7d90b076f68700ac03b0a
ZiwenZhuang <leo416850130@outlook.com>
2020-04-06 00:14:08 +0800

    update bandit EnvInfo for logging metrics

--- girl/envs/bandit.py
+++ girl/envs/bandit.py
@@ -2,9 +2,11 @@ from girl.utils.quick_args import save__init__args
 from girl.envs.base import EnvBase, EnvInfo, EnvStep
 from girl.concepts.spaces.int_box import IntBox
 
+from collections import namedtuple
 import numpy as np
 from typing import List
 
+BanditEnvInfo = namedtuple("BanditEnvInfo", ["V_star", "is_optimal"])
 class BanditEnv(EnvBase):
     """ In the multi-armed bandit problem setting
     """
@@ -15,9 +17,10 @@ class BanditEnv(EnvBase):
         @ Args:
             win_probs: telling the winning probablilty of each arm, which also gives the number of arms
         """
+        win_probs = np.array(win_probs)
         save__init__args(locals(), underscore=True)
         self._action_space = IntBox(0, len(self._win_probs))
-        self._observation_space = IntBox(0, 1) # This surves not purpose, just to meet the interface
+        self._observation_space = IntBox(0, 1) # This serves no purpose, just to meet the interface
 
     def reset(self):
         return self._observation_space.null_value()
@@ -27,9 +30,13 @@ class BanditEnv(EnvBase):
         r = np.random.binomial(1, self._win_probs[a])
         o = self._observation_space.null_value()
 
+        is_optimal = (self._win_probs[a] == np.amax(self._win_probs))
         return EnvStep(
             observation= o,
             reward= r,
             done= False,
-            env_info= EnvInfo(),
+            env_info= EnvInfo(
+                np.amax(self._win_probs), # V_star
+                is_optimal,
+            ),
         )

update interface env_info - env_infos, baseline_table - baselines. And fix bug for usb and gradient bandit

girl/agents/bandit.py
@@ -78,9 +78,14 @@ class ucbBanditAgent(ActionCountAgent):
         action_batch = list()
         for _ in range(self.batch_size):
 
-            ucb_ = torch.sqrt(2 * torch.log(self._t) / self.action_count_table) * self.c
-            ucb = self.q_table + ucb_
-            action = torch.argmax(ucb)
+            # First make sure all actions are experienced once
+            where_zero = np.where(self._action_count == 0)[0]
+            if len(where_zero) > 0:
+                action = torch.tensor(where_zero[0])
+            else:
+                ucb_ = torch.sqrt(2 * torch.log(self._t) / self.action_count_table)
+                ucb = self.q_table + self.c * ucb_
+                action = torch.argmax(ucb)
 
             action_batch.append(action)
         return torch.stack(action_batch)
@@ -144,14 +149,14 @@ class GradientAgent(AgentBase):
             self.baselines = list() # to record history rewards
         super().reset(batch_size= batch_size)
 
-    def update_baseline(self, reward):
+    def update_baselines(self, reward):
         """ NOTE: batch-wise reward
         """
         if self.b is None:
             self.baselines.append(reward)
 
     @property
-    def baseline_table(self):
+    def baselines(self):
         """ A batch of baseline (mean of history reward)
         """
         if self.b is None:

girl/algos/bandit.py
@@ -23,16 +23,16 @@ class BanditAlgo(AlgoBase):
         self.total_regret = np.zeros(tuple())
         self.optimal_counts = np.zeros(tuple())
 
-    def train(self, epoch_i, trajs: Trajectory, env_info):
+    def train(self, epoch_i, trajs: Trajectory, env_infos):
         """ Just to make sure the trajectory length and batch size are both 1
         """
         T, B = trajs.reward.shape[:2] # due to bandit setting, both of these are 1
         assert T == 1 and B == 1, "You sample too much for bandit algorithm T{}-B{}".format(T, B)
 
-    def compute_metrics(self, epoch_i, trajs: Trajectory, env_info):
+    def compute_metrics(self, epoch_i, trajs: Trajectory, env_infos):
         """ Under bandit setting, B == T == 1
         """
-        env_info = env_info[0][0]
+        env_info = env_infos[0][0]
         regret = env_info.V_star - trajs.reward[0, 0].numpy()
         self.total_regret += regret
 
@@ -52,8 +52,8 @@ class eGreedyBandit(BanditAlgo):
     def initialize(self, agent: ActionCountAgent):
         super().initialize(agent)
 
-    def train(self, epoch_i, trajs: Trajectory, env_info):
-        super().train(epoch_i, trajs, env_info)
+    def train(self, epoch_i, trajs: Trajectory, env_infos):
+        super().train(epoch_i, trajs, env_infos)
         action = trajs.action[0, 0]
         self.agent.action_count_table[action] += 1
 
@@ -63,7 +63,7 @@ class eGreedyBandit(BanditAlgo):
         self.agent.q_table[action] += 1./(self.agent.action_count_table[action]) * loss
 
         # Do some logging
-        train_info = self.compute_metrics(epoch_i, trajs, env_info)
+        train_info = self.compute_metrics(epoch_i, trajs, env_infos)
         extra_info = dict(
             action_count= self.agent.action_count_table,
         )
@@ -76,8 +76,8 @@ class ThompsonAlgorithm(BanditAlgo):
     def initialize(self, agent: ThompsonAgent):
         super().initialize(agent)
 
-    def train(self, epoch_i, trajs: Trajectory, env_info):
-        super().train(epoch_i, trajs, env_info)
+    def train(self, epoch_i, trajs: Trajectory, env_infos):
+        super().train(epoch_i, trajs, env_infos)
 
         action = trajs.action[0, 0]
         reward = trajs.reward[0, 0]
@@ -86,7 +86,7 @@ class ThompsonAlgorithm(BanditAlgo):
         self.agent.prior[action, 1] += 1 - reward
 
         # Do some logging
-        train_info = self.compute_metrics(epoch_i, trajs, env_info)
+        train_info = self.compute_metrics(epoch_i, trajs, env_infos)
         extra_info = dict()
         return train_info, extra_info
 
@@ -99,6 +99,7 @@ class GradientBanditAlgo(BanditAlgo):
 
     def initialize(self, agent: GradientAgent):
         super().initialize(agent)
+        self.agent_params = self.agent.parameters()
 
     def loss(self, trajs: Trajectory):
         """ calculate the loss which is used to compute gradient
@@ -106,24 +107,20 @@ class GradientBanditAlgo(BanditAlgo):
         """
         action = trajs.action[0, 0]
         reward = trajs.reward[0] # (B,)
-        baseline_table = self.agent.baseline_table
+        baselines = self.agent.baselines[0]
 
-        indicator = torch.zeros_like(baseline_table)
-        indicator[trajs.action[0,0]] = 1
-
-        pi_loss = (reward - indicator) * self.agent.log_pi_table()[action]
+        pi_loss = (reward - baselines) * self.agent.log_pi_table()[action]
         pi_loss = - pi_loss # for gradient ascent
 
         return pi_loss
 
-    def train(self, epoch_i, trajs: Trajectory, env_info):
-        super().train(epoch_i, trajs, env_info)
+    def train(self, epoch_i, trajs: Trajectory, env_infos):
+        super().train(epoch_i, trajs, env_infos)
 
-        self.agent.update_baseline(trajs.reward[0])
+        self.agent.update_baselines(trajs.reward[0])
 
         # zero_grad
-        params = self.agent.parameters()
-        for param in params:
+        for param in self.agent_params:
             if not param.grad is None:
                 param.grad.data.zero_()
         
@@ -134,11 +131,11 @@ class GradientBanditAlgo(BanditAlgo):
         pi_loss.backward()
 
         # update parameters like gradient descent
-        for param in params:
+        for param in self.agent_params:
             param.data -= self.learning_rate * param.grad.data
 
         # Do some logging
-        train_info = self.compute_metrics(epoch_i, trajs, env_info)
+        train_info = self.compute_metrics(epoch_i, trajs, env_infos)
         extra_info = dict()
         return train_info, extra_info
     

girl/runners/base.py
@@ -8,6 +8,7 @@ from exptools.launching.affinity import set_gpu_from_visibles
 
 import os
 import numbers
+import numpy as np
 import torch
 
 class RunnerBase:
@@ -84,7 +85,7 @@ class RunnerBase:
         logger.log("saved")
 
     def _store_train_info(self, epoch_i,
-            env_info,
+            env_infos,
             train_info,
             extra_info,
         ):
@@ -98,8 +99,10 @@ class RunnerBase:
             new_v = getattr(train_info, k, [])
             v.extend(new_v if isinstance(new_v, list) else [new_v])
         for k, v in self._env_infos.items():
-            new_v = getattr(env_info, k, [])
-            v.extend(new_v if isinstance(new_v, list) else [new_v])
+            for i in env_infos:
+                for env_info in i:
+                    new_v = getattr(env_info, k, [])
+                    v.extend(new_v if isinstance(new_v, list) else [new_v])
 
     def _log_dignostics(self, epoch_i):
         """ Call logger to dump all statistics to the file.
@@ -116,7 +119,8 @@ class RunnerBase:
 
         for k, v in self._env_infos.items():
             # NOTE: incase a value of info is not numeric type
-            if not k.startswith("_") and isinstance(v, numbers.Number):
+            if not k.startswith("_") and len(v) > 0 and \
+                (isinstance(v[0], numbers.Number) or isinstance(v[0], np.ndarray)):
                 logger.record_tabular_misc_stat(k, v, epoch_i)
         self._env_infos = {k: list() for k in self._env_infos}
 
@@ -151,14 +155,14 @@ class RunnerBase:
             self.epoch_i += 1
             # Do the training procedure
             self.agent.sample_mode()
-            env_info = self.sampler.sample(self.epoch_i)
+            env_infos = self.sampler.sample(self.epoch_i)
             self.agent.train_mode()
-            train_info, extra_info = self.algo.train(self.epoch_i, self.sampler.buffer_pyt, env_info)
+            train_info, extra_info = self.algo.train(self.epoch_i, self.sampler.buffer_pyt, env_infos)
             self.agent.sample_mode()
 
             # Do the logging, which is not part of the algorithm
             self._store_train_info(self.epoch_i,
-                env_info,
+                env_infos,
                 train_info,
                 extra_info,
             )